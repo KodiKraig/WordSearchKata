3938e6b1f7840f77fb7201570ee3d3634efce330
Cody Craig <cody.craig@aol.com>
2019-07-17 15:16:18 -0400

    Change to a model to allow parsing in one file scan.

---
+++ Sources/WordSearchKataCore/Model/WordSeachModel.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+public typealias Result<T> = (result: T?, error: String?)
+
+public struct WordSearchModel {
+    
+    static let wordSearchDirectoryName = "WordSearches"
+    
+    public let searchWords: [String]
+    public let grid: [[Character]]
+    
+    public static func build(fromFileAtPath filePath: String) -> Result<WordSearchModel> {
+        // Ensure the file exists
+        if !FileManager.default.fileExists(atPath: filePath) {
+            return (nil, "\(filePath) does not exist")
+        }
+        
+        var searchWords: [String]?
+        var grid: [[Character]]?
+        
+        do {
+            let data = try String(contentsOfFile: filePath, encoding: .utf8)
+            var myStrings = data.components(separatedBy: .newlines)
+            
+            // Parse the search words
+            searchWords = myStrings.remove(at: 0).components(separatedBy: ",").filter({ !$0.isEmpty })
+
+            // Parse the word search grid
+            grid = [[Character]]()
+            for line in myStrings {
+                guard !line.isEmpty else { continue }
+                let newChars = line.components(separatedBy: ",").compactMap({ $0[$0.startIndex] })
+                grid!.append(newChars)
+            }
+        } catch {
+            return (nil, "Unable to parse grid: \(error.localizedDescription)")
+        }
+        
+        guard searchWords != nil, grid != nil else { return (nil, nil) }
+        return (WordSearchModel(searchWords: searchWords!, grid: grid!), nil)
+    }
+}
--- Tests/WordSearchKataTests/Tests/TestWordSearchBuilder.swift
+++ Tests/WordSearchKataTests/Tests/TestWordSearchBuilder.swift
@@ -10,14 +10,28 @@ import XCTest
 import WordSearchKataCore
 
 class TestWordSearchBuilder: XCTestCase {
+    
+    // MARK: build
 
-    func testFileNameDoesNotExist() {
-        XCTAssertEqual(WordSearchBuilder.build(fromFileNamed: "notAWordSearchPuzzle.txt").error, "File notAWordSearchPuzzle.txt does not exist")
+    func testBuild_NameDoesNotExist() {
+        XCTAssertEqual(WordSearchModel.build(fromFileAtPath: "notAWordSearchPuzzle.txt").error, "notAWordSearchPuzzle.txt does not exist")
     }
 
-    func testEmptyWordSearch() {
-        let builderResult = WordSearchBuilder.build(fromFileNamed: TestDataFiles.empty.path)
+    func testBuild_EmptyWordSearch() {
+        let builderResult = WordSearchModel.build(fromFileAtPath: TestDataFiles.empty.path)
         XCTAssertNil(builderResult.error)
-        XCTAssertEqual(builderResult.result!.count, 0)
+        
+        let model = builderResult.result!
+        XCTAssertEqual(model.searchWords.count, 0)
+        XCTAssertEqual(model.grid.count, 0)
+    }
+
+    func testBuild_SmallWordSearch() {
+        let builderResult = WordSearchModel.build(fromFileAtPath: TestDataFiles.smallSingleWorld.path)
+        XCTAssertNil(builderResult.error)
+        
+        let model = builderResult.result!
+        XCTAssertEqual(model.searchWords.count, 4)
+        XCTAssertEqual(model.grid.count, 3)
     }
 }
