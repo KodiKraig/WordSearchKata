3f4b85021d6921055b4d970a36909aa60ad40804
Cody Craig <cody.craig@aol.com>
2019-07-18 01:16:18 -0400

    Refactor

--- Sources/WordSearchKata/main.swift
+++ Sources/WordSearchKata/main.swift
@@ -1,10 +1,13 @@
 import Foundation
 import WordSearchKataCore
 
+fileprivate let defaultWordSearch = "DefaultWordSearch.txt"
+fileprivate let wordSearchDirectory = "WordSearches"
+
 func main() {
     let arguments = ProcessInfo.processInfo.arguments
-    let wordSearchFileName = arguments.count > 1 ? arguments[1] : "DefaultWordSearch.txt"
-    print(App.solveWordSearch(filePath: "\(FileManager.default.currentDirectoryPath)/WordSearches/\(wordSearchFileName)"))
+    let wordSearchFileName = arguments.count > 1 ? arguments[1] : defaultWordSearch
+    print(App.solveWordSearch(filePath: "\(FileManager.default.currentDirectoryPath)/\(wordSearchDirectory)/\(wordSearchFileName)"))
 }
 
 main()
